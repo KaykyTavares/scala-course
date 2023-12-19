e271bfdc63838c0102706a8b0b30288a694efab9
KaykyTavares <kayky.programmer@gmail.com>
2023-12-19 15:10:39 -0300

    dev: explore types and variables declaration

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,26 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  // vals (values) are immutable
+  val x: Int = 42
+  println(x)
+
+  // conpiler can infer types
+  val y = "23"
+  println("Type of y: " + y.getClass)
+
+  // types
+  val aString: String = "Hello"
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4325
+  val aLong: Long = 2132543425L
+  val aFloat: Float - 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+}
